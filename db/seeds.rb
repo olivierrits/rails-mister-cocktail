e22fd3c058b63b1bef489343a782ddc1119874cc
Olivier Rits <olivier.rits@imec.be>
2020-02-21 09:14:20 +0100

    controllers added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data_serialized = open(url).read
+data = JSON.parse(data_serialized)
+
+data["drinks"].each do |drink|
+  # puts drink["strIngredient1"]
+  Ingredient.create(name: drink["strIngredient1"])
+end
